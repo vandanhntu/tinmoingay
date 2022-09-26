c9ae7460cd55088f3ba8beb09097de30e7ebfb83
Kenvin <caovandanhcntt1@gmail.com>
2022-09-26 14:54:27 +0700

    Init source
    Init source

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="p-3 bg-dark text-white" v-if="!isIndex">
+    <div class="container">
+      <div
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
+      >
+        <router-link
+          to="/"
+          class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+        >
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Bongda1" class="mr-2" height="50">
+        </router-link>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><router-link to="/" class="nav-link px-2 text-secondary">Home</router-link></li>
+        </ul>
+
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+    <NuxtPage />
+    </div>
+  </main>
+</template>
+<script setup>
+const isIndex = useRoute().name === 'index';
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    css: ['@/assets/bootstrap/css/bootstrap.min.css']
+    
+})
