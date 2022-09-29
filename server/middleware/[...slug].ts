879437f060b7d68aec231d8f62f1aa3516dd0581
Kenvin <caovandanhcntt1@gmail.com>
2022-09-29 10:17:39 +0700

    z
    z

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -14,6 +14,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://meovat.tinmoingay.net/blog/"+event.req.url);
+        sendRedirect(event,"https://meovat.tinmoingay.net/blog"+event.req.url);
     }
 })
